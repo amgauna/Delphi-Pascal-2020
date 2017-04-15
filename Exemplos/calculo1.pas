814c2dbd6df9febbca1cd63d6c23d21eb62ac4c6
Ana Mercedes Gauna <amgauna@gmail.com>
2017-04-14 22:25:47 -0300

    Create calculo1.pas

---
+++ Exemplos/calculo1.pas
@@ -0,0 +1,9 @@
+program Simples (input, output);
+var
+   X, Y, Soma, Produto : real;
+begin
+   read (X, Y);
+   Soma := X + Y;
+   Produto := X * Y;
+   writeln (´Soma = ´, Soma, ´Produto =´, Produto);
+end.
