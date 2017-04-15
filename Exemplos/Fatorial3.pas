8d98451b07b7a05dd58aeaa9022b22dd016a02e0
Ana Mercedes Gauna <amgauna@gmail.com>
2017-04-14 23:16:40 -0300

    Create Fatorial3.pas

---
+++ Exemplos/Fatorial3.pas
@@ -0,0 +1,11 @@
+(* Programa que recebe um número inteiro positivo e fornece o valor do fatorial desse número usando o comendo for to.  *)
+
+program Fatorial (input, output);
+var
+   Num, Fat, I : integer;
+begin
+   read (Num);
+   Fat := ;
+   for I :=  to Num do Fat := Fat * I;
+   writeln ('Fatorial = ', Fat)
+end.
