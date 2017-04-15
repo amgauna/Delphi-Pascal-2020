21c10c47e627b0053f085880ced37ae974374ca2
Ana Mercedes Gauna <amgauna@gmail.com>
2017-04-14 22:55:49 -0300

    Update Fatorial2.pas

--- Exemplos/Fatorial2.pas
+++ Exemplos/Fatorial2.pas
@@ -1,3 +1,5 @@
+(* Programa que recebe um número inteiro positivo e fornece o valor do fatorial desse número usando o comando repeat until. *)
+
 program Fatorial (input, output);
 var
    Num, Fat, I : integer;
